6f7541714ce3f8e8692be75c72d20c965ecf6723
ilhambonardoo <ilhambonardo123@gmail.com>
2026-02-08 16:42:56 +0700

    membuata contoh data untuk video dan gambar

---
+++ src/lib/data/fishExample.ts
@@ -0,0 +1,21 @@
+export const fishData = [
+  {
+    id: 1,
+    image: "/example/ikancontoh.jpg",
+    title: "IKAN HIAS",
+    description:
+      "Ikan hias berkualitas dengan warna cerah dan kesehatan terjamin",
+  },
+  {
+    id: 2,
+    image: "/example/ikancontoh.jpg",
+    title: "IKAN PREMIUM",
+    description: "Koleksi ikan premium pilihan dengan standar internasional",
+  },
+  {
+    id: 3,
+    image: "/example/ikancontoh.jpg",
+    title: "IKAN LANGKA",
+    description: "Ikan langka dan eksklusif untuk koleksioner sejati",
+  },
+];
---
+++ src/lib/data/ikanMP4.ts
@@ -0,0 +1,46 @@
+import { Video } from "@/src/types/video";
+
+export const ikans: Video[] = [
+  {
+    id: 1,
+    video: "ikan1_pzjtih",
+    label: "ASIATICA RED SPOT KALIMAYA",
+    description:
+      "Ready Satuan & Partai: Mulai dari 10 hingga 1000 ekor\nHarga Jaminan Murah: Langsung dari petani\nKualitas: Ikan sortiran pilihan terbaik\nStok Aman & Siap Kirim Partai Besar/Kecil",
+  },
+  {
+    id: 2,
+    video: "ikan2_ykd0ex",
+    label: "GLOFISH",
+    description:
+      "Ready Satuan & Partai: Mulai dari 10 hingga 1000 ekor\nHarga Jaminan Murah: Langsung dari petani\nKualitas: Ikan sortiran pilihan terbaik\nStok Aman & Siap Kirim Partai Besar/Kecil",
+  },
+  {
+    id: 3,
+    video: "ikan3_mlso5c",
+    label: "STERWARTI ALL SIZE",
+    description:
+      "Ready Satuan & Partai: Mulai dari 10 hingga 1000 ekor\nHarga Jaminan Murah: Langsung dari petani\nKualitas: Ikan sortiran pilihan terbaik\nStok Aman & Siap Kirim Partai Besar/Kecil",
+  },
+  {
+    id: 4,
+    video: "ikan4_ksgnwt",
+    label: "RED BARITO",
+    description:
+      "Ready Satuan & Partai: Mulai dari 10 hingga 1000 ekor\nHarga Jaminan Murah: Langsung dari petani\nKualitas: Ikan sortiran pilihan terbaik\nStok Aman & Siap Kirim Partai Besar/Kecil",
+  },
+  {
+    id: 5,
+    video: "ikan5_pczsld",
+    label: "LIMBATA GRANIT",
+    description:
+      "Ready Satuan & Partai: Mulai dari 10 hingga 1000 ekor\nHarga Jaminan Murah: Langsung dari petani\nKualitas: Ikan sortiran pilihan terbaik\nStok Aman & Siap Kirim Partai Besar/Kecil",
+  },
+  {
+    id: 6,
+    video: "ikan6_g6phoh",
+    label: "STERWARTI ALL SIZE",
+    description:
+      "Ready Satuan & Partai: Mulai dari 10 hingga 1000 ekor\nHarga Jaminan Murah: Langsung dari petani\nKualitas: Ikan sortiran pilihan terbaik\nStok Aman & Siap Kirim Partai Besar/Kecil",
+  },
+];
